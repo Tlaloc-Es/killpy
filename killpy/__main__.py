16b21fc53ea8e861086a7aaee47ca37855657da8
Tlaloc-Es <dev@tlaloc-es.com>
2025-01-16 20:24:01 +0100

    feat: add support for pipx package management and enhance virtual environment tab functionality

--- killpy/__main__.py
+++ killpy/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import platform
 import shutil
 import subprocess
@@ -9,7 +10,31 @@ from pathlib import Path
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.coordinate import Coordinate
-from textual.widgets import DataTable, Footer, Header, Label, Static
+from textual.widgets import (
+    DataTable,
+    Footer,
+    Header,
+    Label,
+    Static,
+    TabbedContent,
+    TabPane,
+)
+
+
+def is_venv_tab(func):
+    def wrapper(self, *args, **kwargs):
+        if self.query_one(TabbedContent).active == "venv-tab":
+            return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+def is_pipx_tab(func):
+    def wrapper(self, *args, **kwargs):
+        if self.query_one(TabbedContent).active == "pipx-tab":
+            return func(self, *args, **kwargs)
+
+    return wrapper
 
 
 def remove_pycache(path: Path) -> int:
@@ -56,6 +81,48 @@ def format_size(size_in_bytes: int):
         return f"{size_in_bytes / (1 << 10):.2f} KB"
     else:
         return f"{size_in_bytes} bytes"
+
+
+def get_pipx_installed_packages():
+    try:
+        result = subprocess.run(
+            ["pipx", "list", "--json"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        installed_packages = json.loads(result.stdout)
+
+        packages_with_size = []
+        for package_name, package_data in installed_packages.get("venvs", {}).items():
+            bin_path = (
+                package_data.get("metadata", {})
+                .get("main_package", {})
+                .get("app_paths", [])[0]
+                .get("__Path__", "")
+            )
+            package_path = Path(bin_path).parent
+            if package_path.exists():
+                total_size = get_total_size(package_path)
+                formatted_size = format_size(total_size)
+                packages_with_size.append((package_name, total_size, formatted_size))
+
+        return packages_with_size
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        raise e
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise e
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        raise e
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise e
 
 
 def find_venvs(base_directory: Path):
@@ -190,29 +257,35 @@ class TableApp(App):
     bytes_release: int = 0
 
     BINDINGS = [
-        Binding(key="ctrl+q", action="quit", description="Quit the app"),
+        Binding(key="ctrl+q", action="quit", description="Exit"),
         Binding(
             key="d",
             action="mark_for_delete",
-            description="Mark .venv for deletion",
+            description="Mark for deletion",
             show=True,
         ),
         Binding(
             key="ctrl+d",
             action="confirm_delete",
-            description="Confirm deletion of marked .venv",
+            description="Delete marked",
             show=True,
         ),
         Binding(
             key="shift+delete",
             action="delete_now",
-            description="Delete the selected .venv immediately",
+            description="Delete immediately",
             show=True,
         ),
         Binding(
             key="p",
             action="clean_pycache",
-            description="Clean __pycache__ directories recursively",
+            description="Clean __pycache__ dirs",
+            show=True,
+        ),
+        Binding(
+            key="u",
+            action="uninstall_pipx",
+            description="Uninstall pipx packages",
             show=True,
         ),
     ]
@@ -222,6 +295,14 @@ class TableApp(App):
         color: white;
         border: heavy green;
     }
+
+    TabbedContent #--content-tab-venv-tab {
+        color: green;
+    }
+
+    TabbedContent #--content-tab-pipx-tab {
+        color: yellow;
+    }
     """
 
     def compose(self) -> ComposeResult:
@@ -230,20 +311,27 @@ class TableApp(App):
             """
 █  ▄ ▄ █ █ ▄▄▄▄  ▄   ▄              ____
 █▄▀  ▄ █ █ █   █ █   █           .'`_ o `;__,
-█ ▀▄ █ █ █ █▄▄▄▀  ▀▀▀█ .       .'.'` '---'  '
-█  █ █ █ █ █     ▄   █  .`-...-'.'A tool to delete virtual environments (.venv, Conda, Poetry)
+█ ▀▄ █ █ █ █▄▄▄▀  ▀▀▀█ .       .'.'` '---'  ' A tool to delete
+█  █ █ █ █ █     ▄   █  .`-...-'.' .venv, Conda, Poetry environments
            ▀      ▀▀▀    `-...-'and clean up __pycache__ and temp files.
         """,
             id="banner",
         )
         yield banner
         yield Label("Searching for virtual environments...")
-        yield DataTable()
-        yield Footer()
+
+        with TabbedContent():
+            with TabPane("Virtual Env", id="venv-tab"):
+                yield DataTable(id="venv-table")
+            with TabPane("Pipx", id="pipx-tab"):
+                yield DataTable(id="pipx-table")
+
+        yield Footer(show_command_palette=False)
 
     async def on_mount(self) -> None:
         self.title = """killpy"""
         await self.find_venvs()
+        await self.find_pipx()
 
     async def find_venvs(self):
         current_directory = Path.cwd()
@@ -257,11 +345,30 @@ class TableApp(App):
         venvs = [env for sublist in venvs for env in sublist]
         venvs = remove_duplicates(venvs)
 
-        table = self.query_one(DataTable)
+        table = self.query_one("#venv-table", DataTable)
         table.focus()
         table.add_columns(
             "Path", "Type", "Last Modified", "Size", "Size (Human Readable)", "Status"
         )
+
+        for venv in venvs:
+            table.add_row(*venv)
+
+        table.cursor_type = "row"
+        table.zebra_stripes = True
+
+        self.query_one(Label).update(f"Found {len(venvs)} .venv directories")
+
+    async def find_pipx(self):
+        venvs = await asyncio.gather(
+            asyncio.to_thread(get_pipx_installed_packages),
+        )
+
+        venvs = [env for sublist in venvs for env in sublist]
+
+        table = self.query_one("#pipx-table", DataTable)
+        table.focus()
+        table.add_columns("Package", "Size", "Size (Human Readable)", "Status")
 
         for venv in venvs:
             table.add_row(*venv)
@@ -278,8 +385,9 @@ class TableApp(App):
         self.query_one(Label).update(f"{format_size(self.bytes_release)} deleted")
         self.bell()
 
+    @is_venv_tab
     def action_confirm_delete(self):
-        table = self.query_one(DataTable)
+        table = self.query_one("#venv-table", DataTable)
         for row_index in range(table.row_count):
             row_data = table.get_row_at(row_index)
             current_status = row_data[5]
@@ -295,8 +403,10 @@ class TableApp(App):
         self.query_one(Label).update(f"{format_size(self.bytes_release)} deleted")
         self.bell()
 
+    @is_venv_tab
     def action_mark_for_delete(self):
-        table = self.query_one(DataTable)
+        table = self.query_one("#venv-table", DataTable)
+
         cursor_cell = table.cursor_coordinate
         if cursor_cell:
             row_data = table.get_row_at(cursor_cell.row)
@@ -310,8 +420,9 @@ class TableApp(App):
                     (cursor_cell.row, 5), EnvStatus.MARKED_TO_DELETE.value
                 )
 
+    @is_venv_tab
     def action_delete_now(self):
-        table = self.query_one(DataTable)
+        table = self.query_one("#venv-table", DataTable)
         cursor_cell = table.cursor_coordinate
         if cursor_cell:
             if cursor_cell in self.deleted_cells:
@@ -326,6 +437,7 @@ class TableApp(App):
             self.deleted_cells.append(cursor_cell)
         self.bell()
 
+    @is_venv_tab
     def delete_environment(self, path, env_type):
         if env_type in {".venv", "pyvenv.cfg", "poetry"}:
             try:
@@ -334,6 +446,34 @@ class TableApp(App):
                 pass
         else:
             remove_conda_env(path)
+
+    @is_pipx_tab
+    def action_uninstall_pipx(self):
+        table = self.query_one("#pipx-table", DataTable)
+        cursor_cell = table.cursor_coordinate
+        if cursor_cell:
+            if cursor_cell in self.deleted_cells:
+                return
+            row_data = table.get_row_at(cursor_cell.row)
+            package = row_data[0]
+            size = row_data[1]
+
+            try:
+                subprocess.run(
+                    ["pipx", "uninstall", package],
+                    check=True,
+                )
+                table.update_cell_at((cursor_cell.row, 3), EnvStatus.DELETED.value)
+                self.deleted_cells.append(cursor_cell)
+                self.bytes_release += size
+                self.query_one(Label).update(
+                    f"{format_size(self.bytes_release)} deleted"
+                )
+            except subprocess.CalledProcessError as e:
+                print(f"Error: {e}")
+                return
+
+        self.bell()
 
 
 def main():
